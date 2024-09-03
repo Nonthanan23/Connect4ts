c5247180182767b73a594abc95078c5055c6ce04
Nonthanan23 <bet.lomsak@gmail.com>
2024-09-03 09:41:52 +0200

    file create and player code

---
+++ typescript/game/Player.ts
@@ -0,0 +1,38 @@
+type Token = 'X' | 'O';
+
+export interface IPlayer {
+    name: string;
+    token: Token;
+    makeMove(board: Board): number;
+}
+
+export class HumanPlayer implements IPlayer {
+    name: string;
+    token: Token;
+    constructor(name: string, token: Token) {
+        this.name = name;
+        this.token = token;
+    }
+    makeMove(board: Board): number {
+        const column = parseInt(prompt(`${this.name}, enter your move (column0-6: `) || '0');
+        return column;
+    }
+}
+
+export class ComputerPlayer implements IPlayer {
+    name: string;
+    token: Token;
+
+    constructor(name: string, token: Token) {
+        this.name = name;
+        this.token = token;
+    }
+
+    makeMove(board: Board): number {
+        let column: number;
+        do {
+            column = Math.floor(Math.random() * board.columns);
+        } while (!board.isColumnValid(column));
+        return column;
+    }
+}
