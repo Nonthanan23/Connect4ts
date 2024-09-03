187f5333c311bf1c6894c99c05f91b2396420c75
Nonthanan23 <bet.lomsak@gmail.com>
2024-09-03 09:50:10 +0200

    index change to game start

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,4 +1,19 @@
-import prompt from './helpers/prompt.js';
+import { Game } from './game/Game';
+import { HumanPlayer, ComputerPlayer } from './game/Player';
 
-let name = prompt('Vad heter du? ');
-console.log(`Hej ${name}!`);
+function main(): void {
+    const player1 = new HumanPlayer('Player 1', 'X');
+    let player2: HumanPlayer | ComputerPlayer;
+
+    const opponentType = prompt('Do you want to play against a computer? (y/n): ') || 'n';
+    if (opponentType.toLowerCase() === 'y') {
+        player2 = new ComputerPlayer('Computer', 'O');
+    } else {
+        player2 = new HumanPlayer('Player 2', 'O');
+    }
+
+    const game = new Game([player1, player2]);
+    game.start();
+}
+
+main();
